25e715a872ac92c4a0f4e60bc922d341f56e555a
keef hassan <keefhassan4@gmail.comgit config --global user.email keefhassan4@gmail.coome>
2023-12-16 21:34:07 -0600

    commit homework 3.3

--- assignment/scripts/part-supply.js
+++ assignment/scripts/part-supply.js
@@ -5,25 +5,31 @@ console.log('****** Part Supply *******');
 
 // 1. Create a variable called 'partsNeeded' with a value of the number 40.
 console.log('1. Create partsNeeded:');
+let partsNeeded = 40;
+console.log(partsNeeded);
 
 
 // 2. Create a variable called 'supplyChanges' whose value is an array containing
 //    the following numbers: 3, 5, -6, 0, 7, 11.
 console.log('2. Create supplyChanges:');
-
+let supplyChanges = [3, 5, -6, 0, 7, 11.]
+console.log(supplyChanges);
 
 // 3. Create a variable called 'secondItem' and assign it the value of the second
 //    item in the 'supplyChanges' array.
 console.log('3. Access the second value of supplyChanges:');
-
-
+let secondItem = (5);
+console.log(secondItem);
 // 4. The last value in the 'supplyChanges' array was added by mistake.
 //    Remove it from the array and store it inside a new variable called 'removedItem'.
 console.log('4. Remove the last value from supplyChanges:');
+supplyChanges.pop();
+let removedItem = (11);
 
 
 // 5. A delivery of 25 more parts arrived. Add the value 25 to the end of the array
 console.log('5. Add the value 25 into supplyChanges.');
+supplyChanges.push(25);
 
 
 // 6. Create three new variables named 'positives', 'negatives', and
@@ -35,9 +41,25 @@ console.log('5. Add the value 25 into supplyChanges.');
 //      - If the value is a zero, push it into the 'zeroes' array.
 console.log('6. Looping through supplyChanges to populate arrays with positive, negative, and zero values:');
 
+let positives = [];
+let negatives = [];
+let zeroes = [];
 
+for( let number of supplyChanges) {
+ if (number > 0) {
+  positives.push(number); 
 
+  } else if (number < 0 ) {
+    negatives.push(number);
+  }else { zeroes.push(number);
 
+  }
+  
+ }
+
+console.log('positive:',[positive]);
+console.log('negatives:',[negatives]);
+console.log('zeroes:',[zeroes]);
 // ***** STRETCH GOALS *********************************************
 // 7. Rewrite the 'for' loop from #6 as a 'for...of' loop. Instead of 'positives',
 //    'negatives', and 'zeroes', create three new arrays named 'stretchPositives',
